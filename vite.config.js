5c1223697e52aed9414c1b3796ac9406c5298974
MadEthan6 <ethan@peostacleaning.com>
2026-07-07 19:07:06 -0700

    Enable javascript-obfuscator for production builds

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import obfuscator from 'vite-plugin-javascript-obfuscator'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    obfuscator({
+      options: {
+        compact: true,
+        controlFlowFlattening: false, // Disabling this makes compilation much faster while keeping obfuscation strong
+        deadCodeInjection: false,
+        debugProtection: false,
+        disableConsoleOutput: false,
+        identifierNamesGenerator: 'hexadecimal',
+        log: false,
+        numbersToExpressions: true,
+        renameGlobals: false,
+        selfDefending: false,
+        simplify: true,
+        splitStrings: true,
+        splitStringsChunkLength: 5,
+        stringArray: true,
+        stringArrayCallsTransform: true,
+        stringArrayEncoding: ['rc4'],
+        stringArrayThreshold: 0.75,
+        unicodeEscapeSequence: false
+      },
+      exclude: [/node_modules/]
+    })
+  ],
   base: '/peosta-cleaning-services/',
 })
